a3b8074ebcce90392eccd4362bbb203b32e2c2af
yxiao54 <62627876+yxiao54@users.noreply.github.com>
2025-03-16 19:37:12 -0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,42 +1,64 @@
-from dataloader import get_data
+import json
 from tqdm import tqdm
-import requests
+import os
+from typing import Dict, Any
+from collections import Counter
+import ollama
+from metric import get_accuracy_finqa,get_f1_tatqa
+from dataloader import get_data
 
 
 
-def query_ollama(question, model="mistral"):
-    url = "http://localhost:11434/api/generate"
-    payload = {"model": model, "prompt": question, "stream": False}
+def create_reader_request_processed(example: Dict[str, Any]):
+    
+    prompt = 'Read the following text and table, and then answer a question:\n'
+    if example['text']:
+        prompt += example['text'] + '\n'
+    prompt += example['table'].strip() + '\n'
+    prompt += 'Question: {}\n'.format(example['question'])
+    prompt += 'Answer:'
+    return prompt
 
-    try:
-        response = requests.post(url, json=payload)
-        if response.status_code == 200:
-            return response.json()["response"].strip()
-        return "ERROR"
-    except Exception as e:
-        print(f"Error querying Ollama: {e}")
-        return "ERROR"
+
+def evaluate(dataset,dataset_name,model_name):
+    all_result=[]
+    all_example=[]
+    for example in tqdm(dataset):
         
-def evaluate(dataset, model="mistral"):
-    correct = 0
-    total = len(dataset)
+        full_prompt = create_reader_request_processed(example)
+        
+        result = ollama.generate(
+                    model=model_name,  # Use an appropriate model
+                    prompt=full_prompt,
+                    options={
+                    "temperature": 0.7,  # Higher temperature for diverse outputs
+                    "num_predict": 512,
+                    "top_p": 1
+                }
+            )
+        
+                
+        all_result.append(result)
+        all_example.append(example)
+        
+    
+    if dataset_name=='finqa':
+        acc=get_accuracy_finqa(all_result,all_example)
+        print(f"This approach achieve {acc} Accuracy on FinQA dataset")
+    elif dataset_name=='tatqa':
+        f1=get_f1_tatqa(all_result,all_example)
+        print(f"This approach achieve {f1} F1 score on TatQA dataset")
 
-    for question, answer,paragraphs,table in tqdm(dataset, desc="Evaluating"):
-        print(question)
-        print(answer)
-        print(table)
-   
-        llm_response = query_ollama(question, model)
-        if llm_response.lower() == str(answer).lower():  # Case-insensitive match
-            correct += 1
-
-    accuracy = (correct / total) * 100
-    print(f"Accuracy: {accuracy:.2f}%")
-    return accuracy
-
+    
+        
+    
+    
 
 if __name__ == "__main__": 
-    dataset=get_data(name='finqa')#finqa,tatqa
-    evaluate(dataset, model="mistral")
+    dataset_name='tatqa'#finqa,tatqa
+    model_name='mistral'
+    dataset=get_data(name=dataset_name)
+    evaluate(dataset, dataset_name,model_name)
+    
 
 
