660201b0e9bf784d5db6dedd9046fe184131a133
yxiao54 <62627876+yxiao54@users.noreply.github.com>
2025-03-21 18:59:43 -0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -4,12 +4,16 @@ import os
 from typing import Dict, Any
 from collections import Counter
 import ollama
-from metric import get_accuracy_finqa,get_f1_tatqa
+from metric import *
 from dataloader import get_data
+from mmqa_utils import (TEXT_SINGLE_HOP_QUESTION_TYPES, TEXT_AS_FIRST_HOP_QUESTION_TYPES,
+                          TEXT_AS_SECOND_HOP_QUESTION_TYPES, TABLE_SINGLE_HOP_QUESTION_TYPES,
+                          TABLE_AS_FIRST_HOP_QUESTION_TYPES, TABLE_AS_SECOND_HOP_QUESTION_TYPES,
+                          IMAGE_SINGLE_HOP_QUESTION_TYPES, IMAGE_AS_FIRST_HOP_QUESTION_TYPES,
+                          IMAGE_AS_SECOND_HOP_QUESTION_TYPES)
 
 
-
-def create_reader_request_processed(example: Dict[str, Any]):
+def create_full_prompt_finqa_tatqa(example: Dict[str, Any]):
     
     prompt = 'Read the following text and table, and then answer a question:\n'
     if example['text']:
@@ -19,13 +23,28 @@ def create_reader_request_processed(example: Dict[str, Any]):
     prompt += 'Answer:'
     return prompt
 
+def create_full_prompt_mmqa(example: Dict[str, Any]):
+    question_type = example["gold_question_type"]
+    prompt = 'Read the following text and table, and then answer a question:\n'
+
+    
+    prompt += '\n'.join(example['table_context']) + '\n'
+    prompt += '\n'.join(example['text_context'])+ '\n'
+    prompt += 'Question: {}\n'.format(example['question'])
+    prompt += 'Answer:'
+    return prompt
 
 def evaluate(dataset,dataset_name,model_name):
     all_result=[]
     all_example=[]
     for example in tqdm(dataset):
-        print(example)
-        full_prompt = create_reader_request_processed(example)
+        
+        if dataset_name in ['finqa','tatqa']:
+            full_prompt = create_full_prompt_finqa_tatqa(example)
+        elif dataset_name in ['mmqa']:
+            full_prompt = create_full_prompt_mmqa(example)
+        else:
+            full_prompt=None
       
         result = ollama.generate(
                     model=model_name,  # Use an appropriate model
@@ -41,6 +60,7 @@ def evaluate(dataset,dataset_name,model_name):
         
         all_result.append(result)
         all_example.append(example)
+     
 
     
     if dataset_name=='finqa':
@@ -49,6 +69,9 @@ def evaluate(dataset,dataset_name,model_name):
     elif dataset_name=='tatqa':
         f1=get_f1_tatqa(all_result,all_example)
         print(f"This approach achieve {f1} F1 score on TatQA dataset")
+    elif dataset_name=='mmqa':
+        f1=get_f1_mmqa(all_result,all_example)
+        print(f"This approach achieve {f1} F1 score on mmqa dataset")
 
     
         
