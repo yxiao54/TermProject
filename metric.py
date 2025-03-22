5a7badf1e082b2549c1e28732a1c90c138a79bbc
yxiao54 <62627876+yxiao54@users.noreply.github.com>
2025-03-21 19:01:30 -0700

    Update metric.py

--- metric.py
+++ metric.py
@@ -5,6 +5,15 @@ from utils import *
 from typing import Dict, Any
 from collections import Counter
 
+from mmqa_utils import (TEXT_SINGLE_HOP_QUESTION_TYPES, TEXT_AS_FIRST_HOP_QUESTION_TYPES,
+                          TEXT_AS_SECOND_HOP_QUESTION_TYPES, TABLE_SINGLE_HOP_QUESTION_TYPES,
+                          TABLE_AS_FIRST_HOP_QUESTION_TYPES, TABLE_AS_SECOND_HOP_QUESTION_TYPES,
+                          IMAGE_SINGLE_HOP_QUESTION_TYPES, IMAGE_AS_FIRST_HOP_QUESTION_TYPES,
+                          IMAGE_AS_SECOND_HOP_QUESTION_TYPES)
+from mmqa_utils import list_em,list_f1
+import numpy as np
+
+
 def parse_api_result(result):
     #to_return = []
     text=result['response']
@@ -92,8 +101,28 @@ def get_f1_tatqa(all_result,all_example):
         if type(pred_answer) == list and type(pred_answer[0]) == str:
             if pred_scale and pred_scale in pred_answer[0]:
                 pred_scale = ''
+        #print(pred_answer)
+        #print(example)
         em_and_f1(ground_truth=example, prediction=pred_answer, pred_scale=pred_scale)
         #example.update({'generated': codes, 'pred_answer': pred_answer, 'pred_scale': pred_scale})
         #writer.write(json.dumps(example) + '\n')
         
         return  em_and_f1.get_overall_metric()[0]
+def get_f1_mmqa(all_result,all_example):
+    all_f1=[]
+    for result,example in zip(all_result,all_example): 
+    
+        gold_answer = [str(it["answer"]) for it in example["answers"]]
+        gold_question_type = example["metadata"]["type"]
+        
+        pred_result=parse_api_result(result)
+  
+ 
+        
+        f1=list_f1(pred_result, gold_answer)
+        em=list_em(pred_result, gold_answer)
+        all_f1.append(f1)
+    f1=np.mean(all_f1)
+    return f1
+        
+ 
